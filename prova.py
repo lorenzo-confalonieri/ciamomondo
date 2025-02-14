27f9336527954ab18d4ebda7f5115b238ee01a9f
lorenzo-confalonieri confalonieri <confalonieri.lorenzo@iisgalvanimi.edu.it>
2025-02-14 11:52:41 +0000

    commit fine ora

--- prova.py
+++ prova.py
@@ -4,3 +4,10 @@ inventario = {
 1 : {"nome": "Nintendo Switch 2", "quantità": 10, "prezzo": 449},
 2 : {"nome": "GTA VI", "quantità": 3, "prezzo": 100}
 }
+def inserisci():
+    chiave = len(inventario) + 1
+    nome = input("inserisci nome: ")
+    quantità = int(input("inserisci la quantità: "))
+    prezzo = int(input("inserisci il prezzo: "))
+    inventario
+    
